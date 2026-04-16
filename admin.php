d28e03a2e2e3b6164992185d47a10a247935e635
asso-oss <asso@hkhk.edu.ee>
2026-04-16 11:34:30 +0300

    s

--- admin.php
+++ admin.php
@@ -3,3 +3,10 @@
 <?php
 
     include("config.php")
+
+    $paring = "SELECT * FROM cars LIMIT 8";
+    $valjund = mysqli_query($yhendus, $paring);
+
+    while($rida = mysqli_fetch_row($valjund)){
+    var_dump($rida[1],$rida[2]);
+}

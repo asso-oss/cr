3810b7b007140e9c562c5d5b5c2260cec114167c
asso-oss <asso@hkhk.edu.ee>
2026-04-17 11:21:22 +0300

    s

--- muuda.php
+++ muuda.php
@@ -13,7 +13,8 @@
         $paring = "SELECT * FROM cars WHERE id=".$_GET['id']."";
         $valjund = mysqli_query($yhendus, $paring);
         // $tulemus = mysqli_affected_rows($yhendus);
-
+        $rida = mysqli_fetch_assoc($valjund);
+        
 
         // if ($tulemus ==1) {
         //     header("Location: admin.php");
@@ -21,12 +22,12 @@
     // }
 ?>
 <h1>Auto lisamine</h1>
-<form action="lisa.php" method="get">
-    Mark <input type="text" name="mark" value="ford"><br>
-    Model <input type="text" name="model" value="focus"><br>
-    Engine <input type="text" name="engine" value="V8"><br>
-    Fuel <input type="text" name="fuel" value="bensiin"><br>
-    Price <input type="text" name="price" value="100"><br>
-    Image <input type="text" name="image" value="ford.jpg"><br>
-    <input type="submit" value="Lisa auto"><br>
+<form action="muuda.php" method="get">
+    Mark <input type="text" name="mark" value="<?php echo $rida['mark']; ?>"><br>
+    Model <input type="text" name="model" value="<?php echo $rida['model']; ?>"><br>
+    Engine <input type="text" name="engine" value="<?php echo $rida['engine']; ?>"><br>
+    Fuel <input type="text" name="fuel" value="<?php echo $rida['fuel']; ?>"><br>
+    Price <input type="text" name="price" value="<?php echo $rida['price']; ?>"><br>
+    Image <input type="text" name="image" value="<?php echo $rida['image']; ?>"><br>
+    <input type="submit" value="Salvesta auto"><br>
 </form>

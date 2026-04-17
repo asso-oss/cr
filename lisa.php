3b281fea3ef205e135244e8a719f6ce377020ac4
asso-oss <asso@hkhk.edu.ee>
2026-04-17 11:48:53 +0300

    u

--- lisa.php
+++ lisa.php
@@ -1,5 +1,5 @@
 <?php
-  include("config.php");
+    include("config.php");
 
     if (!empty($_GET)) {
         $mark = $_GET['mark'];
@@ -9,23 +9,24 @@
         $price = $_GET['price'];
         $image = $_GET['image'];
 
-        $paring = "INSERT INTO cars (mark, model, engine, fuel, price, image) VALUES ('".$mark."', '".$model."', '".$engine."', '".$fuel."', '".$price."', '".$image."')";
+        $paring = "INSERT INTO cars (mark, model, engine, fuel, price, image) 
+        VALUES ( '".$mark."', '".$model."', '".$engine."', '".$fuel."', '".$price."', '".$image."')";
 
         $valjund = mysqli_query($yhendus, $paring);
         $tulemus = mysqli_affected_rows($yhendus);
-
-        if ($tulemus ==1) {
+	    
+        if ($tulemus == 1) {
             header("Location: admin.php");
         }
     }
 ?>
-<h1>Auto lisamine</h1>
+       
 <form action="lisa.php" method="get">
     Mark <input type="text" name="mark" value="ford"><br>
     Model <input type="text" name="model" value="focus"><br>
-    Engine <input type="text" name="engine" value="V8"><br>
+    Engine <input type="text" name="engine" value="v8"><br>
     Fuel <input type="text" name="fuel" value="bensiin"><br>
-    Price <input type="text" name="price" value="100"><br>
+    Price <input type="number" name="price" value="100"><br>
     Image <input type="text" name="image" value="ford.jpg"><br>
     <input type="submit" value="Lisa auto"><br>
-</form>
+</form> 
--- muuda.php
+++ muuda.php
@@ -1,33 +1,45 @@
 <?php
-  include("config.php");
+    include("config.php");
 
-    // if (!empty($_GET)) {
-    //     $mark = $_GET['mark'];
-    //     $model = $_GET['model'];
-    //     $engine = $_GET['engine'];
-    //     $fuel = $_GET['fuel'];
-    //     $price = $_GET['price'];
-    //     $image = $_GET['image'];
+    if (!empty($_GET['muuda_id'])) {
+         $id = $_GET['muuda_id'];
+        $mark = $_GET['mark'];
+        $model = $_GET['model'];
+        $engine = $_GET['engine'];
+        $fuel = $_GET['fuel'];
+        $price = $_GET['price'];
+        $image = $_GET['image'];
+        
+        $paring = "UPDATE cars 
+        SET mark = '".$mark."', 
+        model = '".$model."', 
+        engine = '".$engine."', 
+        fuel = '".$fuel."', 
+        price = '".$price."', 
+        image = '".$image."' 
+        WHERE cars.id = ".$id."";
 
-        // $paring = "INSERT INTO cars (mark, model, engine, fuel, price, image) VALUES ('".$mark."', '".$model."', '".$engine."', '".$fuel."', '".$price."', '".$image."')";
+        print($paring);
+        $valjund = mysqli_query($yhendus, $paring);
+        $tulemus = mysqli_affected_rows($yhendus);
+            if ($tulemus == 1) {
+                header("Location: admin.php");
+            }
+        }
+
         $paring = "SELECT * FROM cars WHERE id=".$_GET['id']."";
         $valjund = mysqli_query($yhendus, $paring);
-        // $tulemus = mysqli_affected_rows($yhendus);
         $rida = mysqli_fetch_assoc($valjund);
-        
-
-        // if ($tulemus ==1) {
-        //     header("Location: admin.php");
-        // }
-    // }
+        print_r($rida);
 ?>
-<h1>Auto lisamine</h1>
+       
 <form action="muuda.php" method="get">
+    <input type="hidden" name="muuda_id" value="<?php echo $rida['id']; ?>"><br>
     Mark <input type="text" name="mark" value="<?php echo $rida['mark']; ?>"><br>
     Model <input type="text" name="model" value="<?php echo $rida['model']; ?>"><br>
     Engine <input type="text" name="engine" value="<?php echo $rida['engine']; ?>"><br>
     Fuel <input type="text" name="fuel" value="<?php echo $rida['fuel']; ?>"><br>
-    Price <input type="text" name="price" value="<?php echo $rida['price']; ?>"><br>
+    Price <input type="number" name="price" value="<?php echo $rida['price']; ?>"><br>
     Image <input type="text" name="image" value="<?php echo $rida['image']; ?>"><br>
     <input type="submit" value="Salvesta auto"><br>
-</form>
+</form> 
